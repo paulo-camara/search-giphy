5849449c854bac5fdb80d42b5183237edd180675
Paulo Victor Fogeiro da Camara <paulo.camara@MIDGARD.LOCAL>
2019-09-04 13:45:27 -0300

    Adicionando link dos gifs

--- src/scripts/components/ScreenSearchGiphy/ScreenSearchGiphy.js
+++ src/scripts/components/ScreenSearchGiphy/ScreenSearchGiphy.js
@@ -1,87 +1,76 @@
-import React from 'react';
-import { InputFilter } from '../Shared/InputFilter/InputFilter';
-import { Gallery } from '../Shared/Gallery/Gallery';
-import { BoxPicture } from '../Shared/BoxPicture/BoxPicture';
+import React from "react";
+import { InputFilter } from "../Shared/InputFilter/InputFilter";
+import { Gallery } from "../Shared/Gallery/Gallery";
+import { BoxPicture } from "../Shared/BoxPicture/BoxPicture";
 
 export class ScreenSearchGiphy extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            data: {
-                valueInput: '',
-            },
-            images: [
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                },
-                {
-                    src:
-                        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                }
-            ]
+    this.state = {
+      data: {
+        valueInput: ""
+      },
+      images: [
+        {
+          src: "https://media.giphy.com/media/d5fdHJvgjQztHqCLhC/giphy.gif"
+        },
+        {
+          src: "https://media.giphy.com/media/iieZBiRO5F9V6/giphy.gif"
+        },
+        {
+          src: "https://media.giphy.com/media/iCTs8CcnOl6KI/giphy.gif"
+        },
+        {
+          src: "https://media.giphy.com/media/bzL59QAV2Pny/giphy.gif"
+        },
+        {
+          src: "https://media.giphy.com/media/3fN8BMRnvIdR6/giphy.gif"
         }
+      ]
+    };
 
-        this.onChangeValue = this.onChangeValue.bind(this);
-        this.onClickFunc = this.onClickFunc.bind(this);
-    }
+    this.onChangeValue = this.onChangeValue.bind(this);
+    this.onClickFunc = this.onClickFunc.bind(this);
+  }
 
-    onChangeValue(event) {
-        this.setState({
-            data: {
-                valueInput: event.target.value
-            }
-        })
-    }
+  onChangeValue(event) {
+    this.setState({
+      data: {
+        valueInput: event.target.value
+      }
+    });
+  }
 
-    onClickFunc() {
-        console.log('Faz a request para a api');
-    }
+  onClickFunc() {
+    console.log("Faz a request para a api");
+  }
 
-    render() {
-        const { valueInput } = this.state.data;
+  render() {
+    const { valueInput } = this.state.data;
 
-        return (
-            <div className="screen-search-giphy">
-                <div className="header">
-                    <InputFilter
-                        className={"screen=search-giphy"}
-                        onChange={this.onChangeValue}
-                        onClick={this.onClickFunc}
-                        value={valueInput}
-                    />
-                    <BoxPicture
-                        image={this.state.images[0]}
-                        onShuffle={() => console.log('Shuffle')}
-                        onCopy={() => console.log('Copy')} />
-                </div>
-                <div className="row">
-                    <Gallery
-                        images={this.state.images}
-                        onOpenPicture={() => console.log('Abre a foto')} />
-                </div>
-            </div>
-        )
-    }
+    return (
+      <div className="screen-search-giphy">
+        <div className="header">
+          <InputFilter
+            className={"screen=search-giphy"}
+            onChange={this.onChangeValue}
+            onClick={this.onClickFunc}
+            value={valueInput}
+          />
+          <BoxPicture
+            image={this.state.images[0]}
+            onShuffle={() => console.log("Shuffle")}
+            onCopy={() => console.log("Copy")}
+          />
+        </div>
+        <div className="row">
+          <Gallery
+            images={this.state.images}
+            onOpenPicture={() => console.log("Abre a foto")}
+          />
+        </div>
+      </div>
+    );
+  }
 }
